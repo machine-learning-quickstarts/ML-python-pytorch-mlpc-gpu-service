09c893ec711e632b6369c90e8a182e8930caed50
Terry Cox <terry@bootstrap.je>
2019-06-18 14:30:46 +0100

    fix: Bind to all interfaces
    Signed-off-by: Terry Cox <terry@bootstrap.je>

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ class StandaloneApplication(gunicorn.app.base.BaseApplication):
 
 if __name__ == '__main__':
     options = {
-        'bind': '%s:%s' % ('127.0.0.1', '8080'),
+        'bind': '%s:%s' % ('0.0.0.0', '8080'),
         'workers': number_of_workers(),
     }
     StandaloneApplication(falcon.API(), options).run()
